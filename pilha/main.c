5f4f8ee0bac4bea58b24911af123a62e72e2d571
Gustavo Fantato <95775375+GustavoFantato@users.noreply.github.com>
2025-09-15 22:12:36 -0300

    Create main.c

---
+++ pilha/main.c
@@ -0,0 +1,31 @@
+#include "stack.h"
+
+
+/*
+
+push(e) --> insercao do elemento e no topo
+
+pop() --> remove e retorna o elemento do topo da pilha
+
+top() ou peek() --> retorna (sem remover) o elemento do topo da pilha
+
+is_empty() --> verifica se a pilha esta vazia
+
+is_full() --> (qnd a pilha é um tamanho fixo) verifica se a pilha está cheia
+
+free_stack() --> libera a memoria alocada na pilha 
+
+*/
+
+
+
+
+int main(){
+
+
+
+
+
+
+    return 0;
+}
